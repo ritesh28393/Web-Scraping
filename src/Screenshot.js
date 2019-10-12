c63cfbef9aa85871f91ca63e77a80a97a46e245b
ritesh raj <ritesh28393@gmail.com>
2019-10-12 22:25:04 +0530

    modify Screenshot

--- src/Screenshot.js
+++ src/Screenshot.js
@@ -2,10 +2,15 @@
 
 const puppeteer = require('puppeteer');
 
-(async() => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://amazon.in');
-  await page.screenshot({path: 'screenshots/amazon.png'});
-  await browser.close();
+const browser = require('./puppeteerFunctions/browser.js');
+const page = require('./puppeteerFunctions/page.js');
+const keyboard = require('./puppeteerFunctions/keyboard.js');
+const mouse = require('./puppeteerFunctions/mouse.js');
+
+(async () => {
+  const browserInstance = await browser.launch(puppeteer, false);
+  const pageInstance = await browser.newPage(browserInstance);
+  await page.goto(pageInstance, 'https://www.amazon.in');
+  await page.screenshot(pageInstance, './screenshots/amazonHome.png', true);
+  await browser.close(browserInstance);
 })();
