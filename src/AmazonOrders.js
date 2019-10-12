4c42e8ea40a9a2a2cdedcb69bc8da9ab7f8f2b2c
ritesh raj <ritesh28393@gmail.com>
2019-10-12 22:08:42 +0530

    modify AmazonOrder.js

--- src/AmazonOrders.js
+++ src/AmazonOrders.js
@@ -12,18 +12,22 @@ const CREDS = require('../AmazonCreds.js');
 (async () => {
     const browserInstance = await browser.launch(puppeteer, false);
     const pageInstance = await browser.newPage(browserInstance);
-    await page.goto(pageInstance, 'https://www.amazon.in/ap/signin?_encoding=UTF8&ignoreAuthState=1&openid.assoc_handle=inflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_signin&switch_account=');
+    await page.goto(pageInstance, 'https://www.amazon.in');
 
+    const SIGNIN_ANCHOR_1 = '#nav-link-accountList';
+    const SIGNIN_ANCHOR_2 = '#nav-flyout-ya-signin > a';
     const EMAIL_INPUT = '#ap_email';
     const EMAIL_CONTINUE_BUTTON = '#continue';
     const PASSWORD_INPUT = '#ap_password';
-    const SINGIN_BUTTON = '#signInSubmit';
+    const SIGNIN_BUTTON = '#signInSubmit';
     const ORDER_ANCHOR = '#nav-orders';
 
+    await mouse.click(pageInstance, SIGNIN_ANCHOR_1);
+    await mouse.clickAndNavigate(pageInstance, SIGNIN_ANCHOR_2);
     await keyboard.type(pageInstance, EMAIL_INPUT, CREDS.email);
     await mouse.clickAndNavigate(pageInstance, EMAIL_CONTINUE_BUTTON);
     await keyboard.type(pageInstance, PASSWORD_INPUT, CREDS.password);
-    await mouse.clickAndNavigate(pageInstance, SINGIN_BUTTON);
+    await mouse.clickAndNavigate(pageInstance, SIGNIN_BUTTON);
     await mouse.clickAndNavigate(pageInstance, ORDER_ANCHOR);
 
     await browser.close(browserInstance);
