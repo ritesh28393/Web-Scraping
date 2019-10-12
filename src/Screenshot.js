82a5c9c2b730157034748784c3eb1b3fa07e8d23
ritesh raj <ritesh28393@gmail.com>
2019-10-12 22:38:19 +0530

    modified Screenshot.js

--- src/Screenshot.js
+++ src/Screenshot.js
@@ -4,11 +4,9 @@ const puppeteer = require('puppeteer');
 
 const browser = require('./puppeteerFunctions/browser.js');
 const page = require('./puppeteerFunctions/page.js');
-const keyboard = require('./puppeteerFunctions/keyboard.js');
-const mouse = require('./puppeteerFunctions/mouse.js');
 
 (async () => {
-  const browserInstance = await browser.launch(puppeteer, false);
+  const browserInstance = await browser.launch(puppeteer, true);
   const pageInstance = await browser.newPage(browserInstance);
   await page.goto(pageInstance, 'https://www.amazon.in');
   await page.screenshot(pageInstance, './screenshots/amazonHome.png', true);
