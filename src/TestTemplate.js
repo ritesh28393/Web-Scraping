4d7ff72b2a31619e61b74055cd2d1aca8a6ef39f
ritesh raj <ritesh28393@gmail.com>
2020-01-07 22:47:57 +1100

    removing previous traverse method and adding dimension method

--- src/TestTemplate.js
+++ src/TestTemplate.js
@@ -20,191 +20,134 @@ const keyboard = require('./puppeteerFunctions/keyboard.js');
 
         const EVALUATIONFUNC = () => {
             var elements = {}; // fullTagName : new ElementInfoObject()
-            elements['nonProcessTags'] = [];
-            elements['logs'] = [];
+            elements['ProcessTags'] = [];
+            elements['Data'] = [];
+            //elements['MetaData'] = [Link, Title, ...];
             //traverse depth first. FIrst process all children and comes to parent
             //before recursive call, traverse top to bottom (all the way) then left to right
             //after recursive call, traverse in left to right (all the way) then goes up
             var tdf = (ele) => {
                 $(ele).contents().each(function () {
-                    //setting identifier
-                    let self = this;
-                    if (self.nodeType != 3 && self.nodeType != 1) {
-                        return;
-                    }
-                    if ($(self).pruneText().length < 1) {
-                        return;
-                    }
-                    if (self.nodeType == 3) {
-                        if ($(self).siblings().length > 0) {
-                            self = $(self).wrap('<p></p>').parent();
+                    if (this.nodeType == 1) {
+                        //Element
+                        elements['ProcessTags'].push($(this).prop('tagName'));
+                        if ($(this).prop('display') != 'none' && $(this).prop('visibility') != 'hidden') {
+                            if ($(this).css('border') == '') {
+                                elements['Data'].push(new AnyElement(
+                                    $(this).offset().left,
+                                    $(this).offset().top,
+                                    $(this).width(),
+                                    $(this).height(),
+                                    undefined,
+                                    $(this).getPath(),
+                                    'B',
+                                ));
+                            }
+                            if (/H[1-6]/.test($(this).prop('tagName'))) {
+                                elements['Data'].push(new AnyElement(
+                                    $(this).offset().left,
+                                    $(this).offset().top,
+                                    $(this).width(),
+                                    $(this).height(),
+                                    $(this).text(),
+                                    $(this).getPath(),
+                                    'H'
+                                ));
+                                return;
+                            }
+                            else if ($(this).prop('tagName') == 'A') {
+                                elements['Data'].push(new AnyElement(
+                                    $(this).offset().left,
+                                    $(this).offset().top,
+                                    $(this).width(),
+                                    $(this).height(),
+                                    $(this).text(),
+                                    $(this).getPath(),
+                                    'A',
+                                    $(this).attr('href')
+                                ));
+                            }
+                            else if ($(this).prop('tagName') == 'IMG') {
+                                elements['Data'].push(new AnyElement(
+                                    $(this).offset().left,
+                                    $(this).offset().top,
+                                    $(this).width(),
+                                    $(this).height(),
+                                    $(this).attr('alt'),
+                                    $(this).getPath(),
+                                    'I',
+                                    $(this).attr('src')
+                                ));
+                            }
+                            else if ($(this).prop('tagName') == 'HR') {
+                                elements['Data'].push(new AnyElement(
+                                    $(this).offset().left,
+                                    $(this).offset().top,
+                                    $(this).width(),
+                                    $(this).height(),
+                                    undefined,
+                                    $(this).getPath(),
+                                    'HR',
+                                ));
+                            }
+                            else if (['SCRIPT', 'NOSCRIPT', 'STYLE', 'FORM', 'SVG'].includes($(this).prop('tagName'))) {
+                                return;
+                            }
                         }
-                        else {
-                            return;
-                        }
                     }
-                    if ($(self).width() < 10 || $(self).height() < 10) {
-                        return;
+                    else if (this.nodeType == 3 && $.trim(this.nodeValue).length) {
+                        //Text
+                        self = $(this).wrap('<span style="color: Red"/>').parent();
+                        elements['Data'].push(new AnyElement(
+                            self.offset().left,
+                            self.offset().top,
+                            self.width(),
+                            self.height(),
+                            self.text(),
+                            self.getPath()
+                        ));
                     }
-                    if (['SCRIPT', 'NOSCRIPT', 'STYLE'].includes($(self).prop('tagName'))) {
-                        return;
-                    }
-                    $(self).setIdentifier();
-                    /*
-                    switch ($(this).prop('tagName')) {
-                        case 'ARTICLE':
-                        case 'SECTION':
-                        case 'FOOTER':
-                        case 'HEADER':
-                        case 'DIV':
-                            $(this).contents().filter(function () { return this.nodeType == 3 && $(this).pruneText().length > 1; }).wrap("<p></p>");
-                        case 'TABLE':
-                        case 'TR':
-                        case 'UL':
-                        case 'OL':
-                            $(this).setIdentifier();
-                            break;
-                        case 'IMG':
-                            //TODO: if the Parent element has no identifier, then add one
-                            break;
-                        case 'P':
-                            let pContent = $(this).pruneText();
-                            $(this).html(pContent);
-                            break;
-                        default:
-                            $(this).children().each(function () {
-                                if (/H[1-6]/.test($(this).prop('tagName'))) {
-                                    $(this).setIdentifier();
-                                    return false;
-                                }
-                            })
-                            break;
-                    }
-                    */
-                    //recursive call
-                    tdf($(self));
-                    switch ($(self).prop('tagName')) {
-                        case 'H1':
-                        case 'H2':
-                        case 'H3':
-                        case 'H4':
-                        case 'H5':
-                        case 'H6':
-                            WriteToElementObject($(self), 'heading');
-                            break;
-                        case 'A':
-                            WriteToElementObject($(self), 'anchor');
-                            $(self).replaceWith($(self).text());
-                            break;
-                        case 'IMG':
-                            WriteToElementObject($(self), 'image');
-                            break;
-                        default:
-                            WriteToElementObject($(self), 'container');
-                            elements['nonProcessTags'].push($(self).prop('tagName'));
-                            break;
-                    }
+                    tdf($(this));
                 });
             };
-            //mark the container parent for a given current element(child)
-            function WriteToElementObject(currentEle, FunctionType) {
-                // parentFullTagName : new ElementInfoObject()
-                let parentFullTagName;
-                currentEle.parents().each(function () {
-                    if ($(this).getIdentifier() != undefined) {
-                        parentFullTagName = $(this).getIdentifier();
-                        elements['logs'].push('prt=>' + parentFullTagName + ' & curr=>' + currentEle.prop('tagName') + ' & Fun=>' + FunctionType);
-                        if (elements[parentFullTagName] == undefined) {
-                            elements[parentFullTagName] = new ElementInfoObject($(this).prop('tagName'));
-                        }
-                        return false;
+            //object
+            function AnyElement(left, top, width, height, text, path, eleType, address) {
+                this.left = left;
+                this.top = top;
+                this.width = width;
+                this.height = height;
+                this.text = text.trim();
+                this.path = path;
+                this.eleType = eleType; // H(heading), I(image), A(anchor), HR(horizontal rule), B(has visible border box)
+                this.address = address;
+            }
+            //jquery custom function
+            $.fn.getPath = function () {
+                if (this.length != 1) throw 'Requires one element.';
+                var path, node = this;
+                while (node.length) {
+                    var realNode = node[0];
+                    var name = (
+                        realNode.localName ||
+                        realNode.tagName ||
+                        realNode.nodeName
+                    );
+                    if (!name || name == '#document') break;
+                    name = name.toLowerCase();
+                    if (realNode.id) {
+                        return name + '#' + realNode.id + (path ? ' > ' + path : '');
+                    } else if (realNode.className) {
+                        name += '.' + realNode.className.split(/\s+/).join('.');
                     }
-                })
-                let text, href;
-                switch (FunctionType) {
-                    case 'heading':
-                        elements[parentFullTagName].headings.push(currentEle.pruneText());
-                        break;
-                    case 'anchor':
-                        text = currentEle.pruneText();
-                        href = currentEle.attr('href');
-                        if (text.length > 1 && href != undefined && !href.startsWith('#')) {
-                            elements[parentFullTagName].anchors.push(new AnchorObject(text, href));
-                        }
-                        break;
-                    case 'image':
-                        elements[parentFullTagName].images.push(new ImageObject(currentEle.attr('alt'), currentEle.attr('src')));
-                        break;
-                    case 'container':
-                        if ($(currentEle).html() == $(currentEle).text()) {
-                            elements[parentFullTagName].texts.push(currentEle.pruneText());
-                            break;
-                        }
-                        childFullTagName = $(currentEle).getIdentifier();
-                        if (elements[childFullTagName] != undefined) {
-                            if (elements[childFullTagName].headings.length > 0 || elements[childFullTagName].children.length > 1) {
-                                elements[parentFullTagName].children.push($(currentEle).getIdentifier());
-                            }
-                            else {
-                                elements['logs'].push('moving data to [' + parentFullTagName + '] from [' + childFullTagName + ']');
-                                elements[childFullTagName].images.reverse().forEach(function (item) {
-                                    elements[parentFullTagName].images.unshift(item);
-                                });
-                                elements[childFullTagName].anchors.reverse().forEach(function (item) {
-                                    elements[parentFullTagName].anchors.unshift(item);
-                                });
-                                elements[childFullTagName].texts.reverse().forEach(function (item) {
-                                    elements[parentFullTagName].texts.unshift(item);
-                                });
-                                elements[childFullTagName].children.reverse().forEach(function (item) {
-                                    elements[parentFullTagName].children.unshift(item);
-                                });
-                                elements[childFullTagName] = undefined;
-                            }
-                        }
-                        break;
+                    var parent = node.parent(), siblings = parent.children(name);
+                    if (siblings.length > 1) name += ':eq(' + siblings.index(node) + ')';
+                    path = name + (path ? ' > ' + path : '');
+                    node = parent;
                 }
-            }
-            //objects
-            function ElementInfoObject(shortTagName) {
-                this.shortTagName = shortTagName; // string
-                this.images = []; // image type
-                this.headings = []; // string type
-                this.anchors = []; // anchor type
-                this.texts = []; // string type
-                this.children = []; // list of fullTagNames(strings) (tagName + '#' + idName + '.' + className)
-            }
-            function ImageObject(alternateText, source) {
-                this.alternateText = alternateText; // string
-                this.source = source; // string
-            }
-            function AnchorObject(text, href) {
-                this.text = text; // string
-                this.href = href; // string
-            }
-            $.fn.extend({
-                setIdentifier: function () {
-                    let tagName = this.prop('tagName');
-                    let randNo = Math.floor(Math.random() * 100) + 1;
-                    let idName = this.attr('id') == undefined ? 'id-' + randNo : this.attr('id');
-                    randNo = Math.floor(Math.random() * 100) + 1;
-                    //let className = this.attr('class') == undefined ? 'cls-' + randNo : this.attr('class');
-                    let className = 'cls-' + randNo;
-                    this.data('IDENTIFIER', tagName + '#' + idName + '.' + className);
-                },
-                getIdentifier: function () {
-                    return this.data('IDENTIFIER');
-                },
-                pruneText: function () {
-                    return this.text().replace(/(\r\n|\n|\r)/gm, "").replace(/\s+/g, ' ').trim();
-                }
-            })
-            $('body').setIdentifier();
+                return path;
+            };
             tdf($('body'));
-            // removing duplicates from elements['nonProcessTags']
-            elements['nonProcessTags'] = elements['nonProcessTags'].filter(function (item, pos) {
-                return elements['nonProcessTags'].indexOf(item) == pos;
-            });
+            elements['ProcessTags'] = elements['ProcessTags'].filter((v, i, a) => a.indexOf(v) === i);
             return elements;
         };
 
