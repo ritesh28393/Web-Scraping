90ab4fd32acb5a3effbebca17e35653eb2218425
ritesh raj <ritesh28393@gmail.com>
2020-01-12 23:49:39 +1100

    grouping started

--- src/TestTemplate.js
+++ src/TestTemplate.js
@@ -11,21 +11,22 @@ const keyboard = require('./puppeteerFunctions/keyboard.js');
     const browserInstance = await browser.launch(puppeteer, true);
     const pageInstance = await browser.newPage(browserInstance);
 
-    //const URL = "https://www.amazon.com.au/Rip-Curl-Womens-Plains-TEE/dp/B07TWB5355/ref=br_asw_pdt-10/355-6876911-7043760?pf_rd_m=ANEGB3WVEVKZB&pf_rd_s=&pf_rd_r=7HHSB45DWCFCWBG12N5D&pf_rd_t=36701&pf_rd_p=34994ded-8732-4a24-9b68-d51f6bcb05fd&pf_rd_i=desktop";
-    const URL = "C:\\Users\\Ritesh Raj\\Desktop\\Temp\\Web-Scrapping\\src\\TestHTMLtemplate\\PlayGround.html";
+    const URL = "https://www.w3schools.com/w3css/tryw3css_templates_blog.htm";
+    //const URL = "C:\\Users\\Ritesh Raj\\Desktop\\Temp\\Web-Scrapping\\src\\TestHTMLtemplate\\PlayGround.html";
 
     try {
         await page.goto(pageInstance, URL);
         await page.waitForSelector(pageInstance, 'body');
 
         const EVALUATIONFUNC = () => {
-            var elements = {}; 
+            var elements = {};
             elements['ProcessTags'] = [];
             elements['Data'] = [];
             //configuration start
             var minWidthElement = 10;
             var minHeightElement = 10;
-            var maxLineWidthForGroupig = 20;
+            var maxWidthForGrouping = 20;
+            var maxHeightForGrouping = 10;
 
             //configuration end
             //traverse depth first. FIrst process parent and then all its children (left to right)
@@ -34,7 +35,7 @@ const keyboard = require('./puppeteerFunctions/keyboard.js');
                     if (this.nodeType == 1 && $(this).css('display') != 'none' && $(this).css('visibility') != 'hidden' && $(this).css('clip') == 'auto' && $(this).width() > minWidthElement && $(this).height() > minHeightElement) {
                         //Element
                         elements['ProcessTags'].push($(this).prop('tagName'));
-                        if (!$(this).css('border').startsWith('0')) {
+                        if (!$(this).css('border').startsWith('0') || $(this).css('box-shadow') != 'none') {
                             //if value is '' that means border-bottom/top/left/right has some value
                             //HR has a border property (It is like a border with 0 height)
                             elements['Data'].push(new AnyElement(
@@ -111,10 +112,11 @@ const keyboard = require('./puppeteerFunctions/keyboard.js');
                 this.top = top;
                 this.width = width;
                 this.height = height;
-                this.text = text.trim();
+                this.text = text ? text.trim() : '';
                 this.path = path;
                 this.eleType = eleType; // H(heading), I(image), A(anchor), B(has visible border box), T(text)
                 this.address = address;
+                this.groupIndex = -1;
             }
             //jquery custom function
             $.fn.getPath = function () {
@@ -142,6 +144,53 @@ const keyboard = require('./puppeteerFunctions/keyboard.js');
                 return path;
             };
             tdf($('body'));
+            //grouping
+            //grouping by text
+            /*
+            groupIndexCounter = 0;                        
+            elements['textGroup'] = elements['Data'].filter(e => e.eleType == 'T');
+            elements['textGroup'][0].groupIndex = groupIndexCounter;
+            for (let index = 0; index < elements['textGroup'].length; index++) {
+                const self = elements['textGroup'][index];
+                const next = index == elements['textGroup'].length ? undefined : elements['textGroup'][index + 1];
+                if (next) {
+                    //next to it and distance between the box is less than maxWidthForGrouping
+                    if (self.top == next.top && next.left - self.left - self.width < maxWidthForGrouping) {
+                        next.groupIndex = self.groupIndex;
+                    }
+                    //below to it and distance between the box is less than maxHeightForGrouping
+                    else if (self.left == next.left && next.top - self.top - self.height < maxHeightForGrouping) {
+                        next.groupIndex = self.groupIndex;
+                    }
+                    else {
+                        next.groupIndex = groupIndexCounter++;
+                    }
+                }
+            }
+            */
+            //grouping by selector path
+            groupIndexCounter = 0;
+            elements['textGroup'] = elements['Data'].filter(e => e.eleType == 'T');
+            elements['textGroup'][0].groupIndex = groupIndexCounter;
+            for (let index = 0; index < elements['textGroup'].length; index++) {
+                const self = elements['textGroup'][index];
+                const next = index == elements['textGroup'].length ? undefined : elements['textGroup'][index + 1];
+                if (next) {
+                    let selfSelector = self.path.split(' > ');
+                    let nextSelector = next.path.split(' > ');
+                    let canBeGrouped = true;
+                    for (let i = 0; i < selfSelector.length - 1; i++) {
+                        // for loop till parent
+                        // not checking if self and next path has same length
+                        if (nextSelector[i] != undefined && selfSelector[i].split(':')[0] != nextSelector[i].split(':')[0]) {
+                            canBeGrouped = false;
+                            break;
+                        }
+                    }
+                    next.groupIndex = canBeGrouped ? self.groupIndex : ++groupIndexCounter;
+                }
+            }
+            //processed tags
             elements['ProcessTags'] = elements['ProcessTags'].filter((v, i, a) => a.indexOf(v) === i);
             return elements;
         };
